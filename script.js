80be71eae6807a404f002a34646844780002585f
momo-gram <125547085+momo-gram@users.noreply.github.com>
2023-11-01 15:10:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,30 @@
-let startTime = 0;
-let intervalId = null;
-let running = false;
+let startTime = 0; // ストップウォッチが開始された時間を格納
+let intervalId = null; // setInterval関数の戻り値を格納
+let running = false; // ストップウォッチが実行中かどうかを示す
 
 function startStopwatch() {
     if (!running) {
-        // スタートボタン押下時処理
-        startTime = Date.now() - (startTime ? startTime : 0);
-        intervalId = setInterval(updateTime, 10); // 10ミリ秒毎更新
-        running = true;
+        // スタートボタンのクリック処理
+        startTime = Date.now() - (startTime ? startTime : 0); // ストップウォッチの開始時刻を設定
+        intervalId = setInterval(updateTime, 10); // 10ミリ秒ごとにupdateTime関数を呼び出して経過時間を更新
+        running = true; // ストップウォッチが実行中であることを示す
         document.getElementById('start').disabled = true; // スタートボタンを無効
-        document.getElementById('stop').disabled = false;
-        document.getElementById('reset').disabled = true;
+        document.getElementById('stop').disabled = false; // ストップボタンを有効
+        document.getElementById('reset').disabled = true; // リセットボタンを無効
     }
 }
 
 function stopStopwatch() {
     if (running) {
-        // ストップボタンが押下時処理
-        clearInterval(intervalId);
-        running = false;
+        // ストップボタンのクリック処理
+        clearInterval(intervalId); // 経過時間の更新を停止
+        running = false; // ストップウォッチが実行中でないことを示す
         document.getElementById('start').disabled = false; // スタートボタンを有効
-        document.getElementById('stop').disabled = true;
-        document.getElementById('reset').disabled = false;
+        document.getElementById('stop').disabled = true; // ストップボタンを無効
+        document.getElementById('reset').disabled = false; // リセットボタンを有効
     }
 }
 
 function resetStopwatch() {
     if (!running) {
-        // リセットボタン押下時処理
-        startTime = 0;
-        updateTime();
-        document.getElementById('start').disabled = false; // スタートボタンを有効
-        document.getElementById('stop').disabled = true;
-        document.getElementById('reset').disabled = true;
-    }
-}
-
-function updateTime() {
-    const currentTime = Date.now();
-    const elapsedTime = new Date(currentTime - startTime);
-    const minutes = elapsedTime.getUTCMinutes();
-    const seconds = elapsedTime.getUTCSeconds();
-    const milliseconds = elapsedTime.getUTCMilliseconds();
-    const hours = Math.floor(minutes / 60);
-    document.querySelector('.time').textContent = `${String(hours).padStart(2, '0')}:${String(minutes % 60).padStart(2, '0')}:${String(seconds).padStart(2, '0')}:${String(milliseconds).padStart(3, '0')}`;
-}
+        // リセットボタンのクリック処理
