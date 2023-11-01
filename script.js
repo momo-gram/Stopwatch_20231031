13cb4a5b54d82c219924515b35d921ebfddc996f
momo-gram <125547085+momo-gram@users.noreply.github.com>
2023-11-01 16:39:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,41 @@
 // script.js
-let timerInterval;
-let running = false;
+const startButton = document.getElementById('start');
+const stopButton = document.getElementById('stop');
+const resetButton = document.getElementById('reset');
+const timeDisplay = document.getElementById('time');
+
+let startTime;
 let elapsedTime = 0;
+let timerInterval;
 
-function startStopwatch() {
-  if (!running) {
-    running = true;
-    document.getElementById("start").disabled = true;
-    document.getElementById("stop").disabled = false;
-    document.getElementById("reset").disabled = true;
-    timerInterval = setInterval(updateTime, 10);
-  }
+function startTimer() {
+  startButton.disabled = true; // スタートボタンを無効にする
+  stopButton.disabled = false; // ストップボタンを有効にする
+  startTime = Date.now() - elapsedTime;
+  timerInterval = setInterval(updateTime, 10);
 }
 
-function stopStopwatch() {
-  if (running) {
-    running = false;
-    document.getElementById("start").disabled = false;
-    document.getElementById("stop").disabled = true;
-    document.getElementById("reset").disabled = false;
-    clearInterval(timerInterval);
-  }
+function stopTimer() {
+  startButton.disabled = false; // スタートボタンを有効にする
+  stopButton.disabled = true; // ストップボタンを無効にする
+  clearInterval(timerInterval);
 }
 
-function resetStopwatch() {
-  if (!running) {
-    elapsedTime = 0;
-    updateTime();
-  }
+function resetTimer() {
+  startButton.disabled = false; // スタートボタンを有効にする
+  stopButton.disabled = true; // ストップボタンを無効にする
+  clearInterval(timerInterval);
+  elapsedTime = 0;
+  updateTime();
 }
 
 function updateTime() {
-  const centiseconds = String(elapsedTime % 100).padStart(2, "0");
-  const seconds = String(Math.floor((elapsedTime / 100) % 60)).padStart(2, "0");
-  const minutes = String(Math.floor((elapsedTime / 6000) % 60)).padStart(2, "0");
-  const hours = String(Math.floor(elapsedTime / 360000)).padStart(2, "0");
-  document.getElementById("stopwatch").textContent = `${hours}:${minutes}:${seconds}:${centiseconds}`;
-  elapsedTime += 1;
+  const currentTime = Date.now();
+  elapsedTime = currentTime - startTime;
+  const formattedTime = formatTime(elapsedTime);
+  timeDisplay.innerText = formattedTime;
 }
 
-updateTime();
+function formatTime(milliseconds) {
+  const date = new Date(milliseconds);
+  const minutes = date
