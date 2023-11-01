5ec972a0b91d4e00a9b1bdf921e88dc81b274d35
momo-gram <125547085+momo-gram@users.noreply.github.com>
2023-11-01 14:44:56 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,70 +1,40 @@
-var startButton;    // startボタン
-var stopButton;     // stopボタン
-var resetButton;    // resetボタン
-var showTime;       // 表示時間
+let startTime = 0;
+let intervalId = null;
+let running = false;
 
-var timer;              // setinterval, clearTimeoutで使用
-var startTime;          // 開始時間
-var elapsedTime = 0;    // 経過時間
-var holdTime = 0;       // 一時停止用に時間を保持
-
-window.onload = function () {
-    startButton = document.getElementById("start");
-    stopButton = document.getElementById("stop");
-    resetButton = document.getElementById("reset");
-    showTime = document.getElementById("time");
+function startStopwatch() {
+    if (!running) {
+        startTime = Date.now() - (startTime ? startTime : 0);
+        intervalId = setInterval(updateTime, 1000);
+        running = true;
+        document.getElementById('start').disabled = true;
+        document.getElementById('stop').disabled = false;
+    }
 }
 
-// スタートボタン押
-function start(){
-    // 開始時間を現在の時刻に設定
-    startTime = Date.now();
-
-    // 時間計測
-    measureTime();
-
-    startButton.disabled = true;
-    stopButton.disabled = false;
-    resetButton.disabled = false;
+function stopStopwatch() {
+    if (running) {
+        clearInterval(intervalId);
+        running = false;
+        document.getElementById('start').disabled = false;
+        document.getElementById('stop').disabled = true;
+    }
 }
 
-// ストップボタン押
-function stop(){
-    // タイマー停止
-    clearInterval(timer);
-
-    // 停止時間を保持
-    holdTime += Date.now() - startTime;
-
-    startButton.disabled = false;
-    stopButton.disabled = true;
-    resetButton.disabled = false;
+function resetStopwatch() {
+    if (!running) {
+        startTime = 0;
+        updateTime();
+        document.getElementById('start').disabled = false;
+        document.getElementById('stop').disabled = true;
+    }
 }
 
-// リセットボタン押
-function reset(){
-    // タイマー停止
-    clearInterval(timer);
-
-    // 変数、表示を初期化
-    elapsedTime = 0;
-    holdTime = 0;
-    showTime.textContent = "00:00.000";
-
-    startButton.disabled = false;
-    stopButton.disabled = true;
-    resetButton.disabled = true;
-}
-
-// 時間を計測
-function measureTime() {
-    // タイマーを設定
-    timer = setTimeout(function () {
-        // 経過時間を設定し、画面へ表示
-        elapsedTime = Date.now() - startTime + holdTime;
-        showTime.textContent = new Date(elapsedTime).toISOString().slice(14, 23);
-        
-        // 関数を呼び出し、時間計測を継続する
-        measureTime();
-    }, 10);
+function updateTime() {
+    const currentTime = Date.now();
+    const elapsedTime = new Date(currentTime - startTime);
+    const hours = elapsedTime.getUTCHours();
+    const minutes = elapsedTime.getUTCMinutes();
+    const seconds = elapsedTime.getUTCSeconds();
+    document.querySelector('.time').textContent = `${hours}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
 }
