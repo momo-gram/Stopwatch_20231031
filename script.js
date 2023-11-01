c6b2d6ac0864822a594139a81c118ff0969a7e56
momo-gram <125547085+momo-gram@users.noreply.github.com>
2023-11-01 17:34:53 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
-// リセットボタンのクリックイベントをリセット関数に関連付ける
+let startTime;
+let interval;
+let isRunning = false;
+
+const timeDisplay = document.querySelector(".time");
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const resetButton = document.getElementById("reset");
+
+startButton.addEventListener("click", start);
+stopButton.addEventListener("click", stop);
 resetButton.addEventListener("click", reset);
 
+function start() {
+  if (!isRunning) {
+    startTime = Date.now() - (interval || 0);
+    interval = setInterval(updateTime, 10);
+    isRunning = true;
+    startButton.disabled = true;
+  }
+}
+
+function stop() {
+  if (isRunning) {
+    clearInterval(interval);
+    isRunning = false;
+    startButton.disabled = false;
+  }
+}
+
 function reset() {
-  // インターバルをクリア
   clearInterval(interval);
   isRunning = false;
   startButton.disabled = false;
-
-  // startTimeとintervalを0に戻す
-  startTime = 0;
   interval = 0;
-
-  // タイマーを0にセット
+  startTime = 0;
   updateTime();
 }
+
+function updateTime() {
+  const elapsedTime = Date.now() - startTime;
+  const minutes = Math.floor(elapsedTime / 60000);
+  const seconds = Math.floor((elapsedTime % 60000) / 1000);
+  const milliseconds = (elapsedTime % 1000).toString().slice(0, 2);
+  timeDisplay.textContent = `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0
