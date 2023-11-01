fe9c4ac7244b68781f7bd2c8b7950d5c8034ae5a
momo-gram <125547085+momo-gram@users.noreply.github.com>
2023-11-01 15:02:13 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,40 +6,42 @@ function startStopwatch() {
     if (!running) {
         // スタートボタンがクリックされたときの処理
         startTime = Date.now() - (startTime ? startTime : 0);
-        intervalId = setInterval(updateTime, 1000);
+        intervalId = setInterval(updateTime, 10); // 10ミリ秒ごとに更新
         running = true;
-        document.getElementById('start').disabled = true;
+        document.getElementById('start').disabled = true; // スタートボタンを無効に
         document.getElementById('stop').disabled = false;
+        document.getElementById('reset').disabled = true;
     }
 }
 
 function stopStopwatch() {
     if (running) {
-        // ストップボタンがクリックされたときの処理
+        // ストップボタンが押下時処理
         clearInterval(intervalId);
         running = false;
-        document.getElementById('start').disabled = false;
+        document.getElementById('start').disabled = false; // スタートボタンを有効に
         document.getElementById('stop').disabled = true;
+        document.getElementById('reset').disabled = false;
     }
 }
 
-function startStopwatch() {
+function resetStopwatch() {
     if (!running) {
-        // リセットボタンがクリックされたときの処理
-        startTime = Date.now() - (startTime ? startTime : 0);
-        intervalId = setInterval(updateTime, 1000);
-        running = true;
-        document.getElementById('start').disabled = true;
-        document.getElementById('stop').disabled = false;
-        document.getElementById('reset').disabled = true; // リセットボタンも無効に
+        // リセットボタン押下時処理
+        startTime = 0;
+        updateTime();
+        document.getElementById('start').disabled = false; // スタートボタンを有効に
+        document.getElementById('stop').disabled = true;
+        document.getElementById('reset').disabled = true;
     }
 }
+
 function updateTime() {
     const currentTime = Date.now();
     const elapsedTime = new Date(currentTime - startTime);
-    const hours = elapsedTime.getUTCHours();
     const minutes = elapsedTime.getUTCMinutes();
     const seconds = elapsedTime.getUTCSeconds();
-    // 経過時間を表示
-    document.querySelector('.time').textContent = `${hours}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+    const milliseconds = elapsedTime.getUTCMilliseconds();
+    const hours = Math.floor(minutes / 60);
+    document.querySelector('.time').textContent = `${String(hours).padStart(2, '0')}:${String(minutes % 60).padStart(2, '0')}:${String(seconds).padStart(2, '0')}:${String(milliseconds).padStart(3, '0')}`;
 }
