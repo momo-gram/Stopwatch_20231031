f7b142d39b688679b6742d14b67048a4d397fcdd
momo-gram <125547085+momo-gram@users.noreply.github.com>
2023-11-01 15:53:42 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,44 @@
 let startTime = 0;
 let intervalId = null;
 let running = false;
-let buttonClicked = false;
 
 function startStopwatch() {
-    if (!running && !buttonClicked) {
-        startTime = Date.now() - (running ? Date.now() - startTime : 0);
-        intervalId = setInterval(updateTime, 10);
+    if (!running) {
+        // スタートボタンがクリックされたときの処理
+        startTime = Date.now() - (startTime ? startTime : 0);
+        intervalId = setInterval(updateTime, 1000);
         running = true;
-        buttonClicked = true;
         document.getElementById('start').disabled = true;
         document.getElementById('stop').disabled = false;
-        document.getElementById('reset').disabled = true;
     }
 }
 
 function stopStopwatch() {
     if (running) {
+        // ストップボタンがクリックされたときの処理
         clearInterval(intervalId);
         running = false;
-        buttonClicked = false;
         document.getElementById('start').disabled = false;
         document.getElementById('stop').disabled = true;
-        document.getElementById('reset').disabled = false;
     }
 }
 
 function resetStopwatch() {
     if (!running) {
-        clearInterval(intervalId);
+        // リセットボタンがクリックされたときの処理
         startTime = 0;
         updateTime();
-        buttonClicked = false;
         document.getElementById('start').disabled = false;
         document.getElementById('stop').disabled = true;
-        document.getElementById('reset').disabled = true;
     }
 }
 
 function updateTime() {
     const currentTime = Date.now();
     const elapsedTime = new Date(currentTime - startTime);
+    const hours = elapsedTime.getUTCHours();
     const minutes = elapsedTime.getUTCMinutes();
     const seconds = elapsedTime.getUTCSeconds();
-    const milliseconds = Math.floor(elapsedTime.getUTCMilliseconds() / 10);
-    const hours = Math.floor(minutes / 60);
-    document.querySelector('.time').textContent = `${String(hours).padStart(2, '0')}:${String(minutes % 60).padStart(2, '0')}:${String(seconds).padStart(2, '0')}:${String(milliseconds).padStart(2, '0')}`;
+    // 経過時間を表示
+    document.querySelector('.time').textContent = `${hours}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
 }
