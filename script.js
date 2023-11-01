43240efcd9064b14dd7ad378b0b137596ba0fc78
momo-gram <125547085+momo-gram@users.noreply.github.com>
2023-11-01 15:49:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let startTime = 0;
 let intervalId = null;
 let running = false;
+let buttonClicked = false;
 
 function startStopwatch() {
-    if (!running) {
+    if (!running && !buttonClicked) {
         startTime = Date.now() - (running ? Date.now() - startTime : 0);
         intervalId = setInterval(updateTime, 10);
         running = true;
+        buttonClicked = true;
         document.getElementById('start').disabled = true;
         document.getElementById('stop').disabled = false;
         document.getElementById('reset').disabled = true;
@@ -17,6 +19,7 @@ function stopStopwatch() {
     if (running) {
         clearInterval(intervalId);
         running = false;
+        buttonClicked = false;
         document.getElementById('start').disabled = false;
         document.getElementById('stop').disabled = true;
         document.getElementById('reset').disabled = false;
@@ -28,6 +31,7 @@ function resetStopwatch() {
         clearInterval(intervalId);
         startTime = 0;
         updateTime();
+        buttonClicked = false;
         document.getElementById('start').disabled = false;
         document.getElementById('stop').disabled = true;
         document.getElementById('reset').disabled = true;
@@ -41,5 +45,4 @@ function updateTime() {
     const seconds = elapsedTime.getUTCSeconds();
     const milliseconds = Math.floor(elapsedTime.getUTCMilliseconds() / 10);
     const hours = Math.floor(minutes / 60);
-    document.querySelector('.time').textContent = `${String(hours).padStart(2, '0')}:${String(minutes % 60).padStart(2, '0')}:${String(seconds).padStart(2, '0')}:${String(milliseconds).padStart(2, '0')}`;
-}
+    document.querySelector('.time').textContent =
